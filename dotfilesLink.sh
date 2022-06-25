5447b7c5392c4c283fb7d1c025897a115c418f77
yud-yahara <yahara.yudai.16@shizuoka.ac.jp>
2022-06-25 17:18:36 +0900

    install vim-plug and colors

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,3 +7,7 @@ ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
 mkdir -p ~/.vim/undo
+mkdir -p ~/.vim/colors
+git clone https://github.com/tomasr/molokai
+mv molokai/colors/molokai.vim ~/.vim/colors/
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
